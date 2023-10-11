5a86b297b7e3066c5e5dd39be3a1fb4bfe94218c
Elis Memic <elismemic5@gmail.com>
2023-10-11 11:55:11 +0200

    Add 4 missions, remove restriction for 3

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -38,36 +38,33 @@ const Missions = () => {
               <tr key={mission.id} className={index % 2 === 0 ? 'bg-gray-100' : ''}>
                 <td className="border px-4 py-2">{mission.name}</td>
                 <td className="border px-4 py-2">{mission.description}</td>
-                {index !== 3 ? (
-                  <>
-                    <td className="border px-4 py-2 w-1/6">
-                      {mission.joined ? (
-                        <span className="bg-blue-500 text-white w-auto rounded-md p-1 mr-2">Active Member</span>
-                      ) : (
-                        <span className="bg-gray-500 text-white w-auto rounded-md p-1 mr-2">NOT A MEMBER</span>
-                      )}
-                    </td>
-                    <td className="border px-4 py-2 w-1/6">
-                      {mission.joined ? (
-                        <button
-                          className="border border-red-500 hover:bg-red-200 text-red-500 py-2 px-4 rounded-md"
-                          type="button"
-                          onClick={() => handleCancelClick(mission.id)}
-                        >
-                          Leave Mission
-                        </button>
-                      ) : (
-                        <button
-                          className="border border-gray-500 hover:bg-gray-200 text-gray-500 py-2 px-4 rounded-md"
-                          type="button"
-                          onClick={() => handleJoinClick(mission.id)}
-                        >
-                          Join Mission
-                        </button>
-                      )}
-                    </td>
-                  </>
-                ) : null}
+
+                <td className="border px-4 py-2 w-1/6">
+                  {mission.joined ? (
+                    <span className="bg-blue-500 text-white w-auto rounded-md p-1 mr-2">Active Member</span>
+                  ) : (
+                    <span className="bg-gray-500 text-white w-auto rounded-md p-1 mr-2">NOT A MEMBER</span>
+                  )}
+                </td>
+                <td className="border px-4 py-2 w-1/6">
+                  {mission.joined ? (
+                    <button
+                      className="border border-red-500 hover:bg-red-200 text-red-500 py-2 px-4 rounded-md"
+                      type="button"
+                      onClick={() => handleCancelClick(mission.id)}
+                    >
+                      Leave Mission
+                    </button>
+                  ) : (
+                    <button
+                      className="border border-gray-500 hover:bg-gray-200 text-gray-500 py-2 px-4 rounded-md"
+                      type="button"
+                      onClick={() => handleJoinClick(mission.id)}
+                    >
+                      Join Mission
+                    </button>
+                  )}
+                </td>
               </tr>
             ))}
           </tbody>
